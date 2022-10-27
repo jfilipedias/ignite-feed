85f5c39a9639a9793de5b26f6fbd643cbf5ba3b9
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-10-27 16:07:07 -0300

    Implements the Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -9,12 +11,20 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/jfilipedias.png"
+        />
+
         <strong>Filipe Dias</strong>
         <span>Front-end Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   )
