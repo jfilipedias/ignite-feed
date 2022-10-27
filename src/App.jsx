dc24c1e185ffdbec7ab81f17ba5180618a206085
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-10-26 22:13:49 -0300

    Starts to implement the Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
+import { Post } from './components/Post'
+
+import styles from './App.module.css'
+
 import './styles.css'
 
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Filipe Dias"
+            content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          />
+        </main>
+      </div>
+    </>
+  )
 }
