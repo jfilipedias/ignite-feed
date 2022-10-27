c1804c71ddcb4e7d64343c2008eca43e02c9fd1e
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-10-27 17:06:38 -0300

    Starts to implements the Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,23 @@
-export function Post(props) {
+import styles from './Post.module.css'
+
+export function Post() {
   return (
-    <>
-      <h2>{props.author}</h2>
-      <p>{props.content}</p>
-    </>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/jfilipedias.png"
+          />
+
+          <div className={styles.authorInfo}>
+            <strong>Filipe Dias</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time dateTime="">Publicado há 1h</time>
+      </header>
+    </article>
   )
 }
