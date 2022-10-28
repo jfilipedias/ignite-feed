d1f4f260b09525557ba3ce87668cf4198a06b805
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-10-28 17:00:19 -0300

    Starts to implements the comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,38 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/jfilipedias.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.commentInfo}>
+              <strong>Filipe Dias</strong>
+              <time
+                title="28 de Outubro às 00:13"
+                dateTime="2022-10-28T00:13:38.000Z"
+              >
+                Publicado há 1h
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash siz={24} />
+            </button>
+          </header>
+
+          <p>Muito bom, parabéns!!! </p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp /> Curtir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
