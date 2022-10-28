7259cd34958b4c9e9f70520272aaf3578b1f0080
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-10-28 09:17:28 -0300

    Styles the post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -22,25 +22,25 @@ export function Post() {
         >
           Publicado há 1h
         </time>
-
-        <div className={styles.content}>
-          <p>Fala galeraa 👋</p>
-
-          <p>
-            Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-            no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare
-            🚀
-          </p>
-
-          <p>
-            👉<a href="">jane.design/doctorcare</a>
-          </p>
-
-          <p>
-            <a href="">#novoprojeto #nlw #rocketseat</a>
-          </p>
-        </div>
       </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+
+        <p>
+          <a href="">filipe.design/doctorcare</a>
+        </p>
+
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
     </article>
   )
 }
