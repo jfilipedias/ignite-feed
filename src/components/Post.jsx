d45e512bff52cb27e7171f8546d79ed9bd0ec3f9
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-10-29 22:47:03 -0300

    Implements the Avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
@@ -6,10 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-            className={styles.avatar}
-            src="https://github.com/jfilipedias.png"
-          />
+          <Avatar src="https://github.com/jfilipedias.png" />
 
           <div className={styles.authorInfo}>
             <strong>Filipe Dias</strong>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { PencilLine } from 'phosphor-react'
 
+import { Avatar } from './Avatar'
 import styles from './Sidebar.module.css'
 
 export function Sidebar() {
@@ -11,10 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/jfilipedias.png"
-        />
+        <Avatar src="https://github.com/jfilipedias.png" />
 
         <strong>Filipe Dias</strong>
         <span>Front-end Developer</span>
